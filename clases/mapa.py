af476c9be29f3a0e29982fb5d8680425cc0c2a56
Jimena Velázquez <jimena.avelazquezlez@gmail.com>
2025-07-19 12:02:19 -0300

    Agregue el primer avance (Asi se vera el mapa en 30x30)

--- clases/mapa.py
+++ clases/mapa.py
@@ -1,1 +1,27 @@
 # Sistema de mapas
+# class Mapa:
+# def init(self, nombre, grid, npcs, items):
+# self.nombre = nombre
+# self.grid = grid # Matriz 2D
+# self.npcs = npcs
+# self.items = items
+# self.conexiones = {} # Mapas conectados
+
+## Prueba 1: Estructura basica del mapa, lograr que se vea.
+### Por decision del team lead, el mapa sera 30x30
+
+class Mapa:
+    def __init__(self, nombre_mapa):
+        self.nombre = nombre_mapa
+        self.grid = [['.' for _ in range(30)] for _ in range(30)]
+        self.npcs = []
+        self.items = []
+        self.conexiones_mapas = {1: '2do Mapa', 2: '3er Mapa', 3:'4to Mapa', 4:'Fin'}
+
+    def mostrar(self):
+        for x in self.grid:
+            print(' '.join(x))
+        print()
+
+mapa1 = Mapa('Inicio')
+mapa1.mostrar()
