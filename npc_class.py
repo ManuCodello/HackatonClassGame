fad829f1e93c4bb2c48b8406415bfcf256580fc4
Tiago RCaligaris <rctiago02@gmail.com>
2025-07-19 13:49:00 -0300

    Creacion de funciones, falta agregar en base a los acertijos o minijuegos

--- npc_class.py
+++ npc_class.py
@@ -1,2 +1,19 @@
-class Npc:
-    pass
+class NPC:
+    def __init__(self, nombre, x, y):
+        self.nombre = nombre
+        self.x = x
+        self.y = y
+        self.dialogo = []
+
+    def hablar(self,):
+
+        pass
+
+    def acertijo(self,):
+        pass
+
+    def entregar_llave(self,):
+        pass
+
+    def moverse(self,):
+        pass
